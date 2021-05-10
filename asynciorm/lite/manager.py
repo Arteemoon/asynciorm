84f23348ec9c1fd5021e564831eeca775db87ac4
Artemon <ivashenco45@gmail.com>
2021-05-10 19:29:04 +0300

    [feature] adapt sqlite manager

--- asynciorm/lite/manager.py
+++ asynciorm/lite/manager.py
@@ -2,4 +2,19 @@ from asynciorm.base.manager import BaseManager
 
 
 class SQLiteManager(BaseManager):
-    pass
+
+    async def fetchone(self):
+        cursor = await self._connector.execute(self.query)
+        return await cursor.fetchone()
+
+    async def fetchall(self):
+        cursor = await self._connector.execute(self.query)
+        return await cursor.fetchall()
+
+    async def execute(self):
+        await self._connector.execute(self.query)
+        await self._connector.commit()
+
+    async def fetch_row(self):
+        cursor = await self._connector.execute(self.query)
+        return await cursor.fetchall()
